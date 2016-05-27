a0b4de3c1c4abf71042ac233d699db8e4292c2c2
0xFireball <0xFireball@outlook.com>
2016-05-26 23:17:45 -0700

    use x86 notice

--- dnSpy-x86/Properties/AssemblyInfo.cs
+++ dnSpy-x86/Properties/AssemblyInfo.cs
@@ -2,9 +2,11 @@
 using System.Resources;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyTitle("deAtomizer-x86")]
 [assembly: AssemblyDescription("deAtomizer - A tweaked, modded version of dnSpy")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ExaPhaser Industries")]
+[assembly: AssemblyProduct("deAtomizer-x86")]
 [assembly: AssemblyCopyright("deAtomizer - Copyright (C) 2016 0xFireball, ExaPhaser Industries <0xFireball@outlook.com>  dnSpy - Copyright (C) 2014-2016 de4dot@gmail.com")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
